a5469d6a120b4a2234560e636b82d038ceaed34a
potureddigowtham <potureddigowtham@gmail.com>
2018-04-29 15:33:14 +0530

    multiplication tables.cpp added

---
+++ multiplication tables.cpp
@@ -0,0 +1,16 @@
+// multiplication tables
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	for(int i=1;i<=10;i++)
+	{
+		for(int j=0;j<=10;j++)
+		{
+			printf("\n %d * %d = %d",i,j,i*j);
+		}
+		printf("\n-------------------\n");
+	}
+}
