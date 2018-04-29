a5c7be8c5657a406f8b45dc3fe1bf387aad96d3c
potureddigowtham <potureddigowtham@gmail.com>
2018-04-29 15:14:53 +0530

    new file:   escape sequence.cpp

---
+++ escape sequence.cpp
@@ -0,0 +1,18 @@
+//first programm
+
+//1. escape sequence
+
+#include <stdio.h>
+
+int main()
+{
+	// \n is for next line.
+	// \t is for next tab.
+	printf("  hi this is P.Gowtham");
+	printf("\n Address: vijayawada.");
+	printf("\n AGE : 20");
+	printf("\n gender: male");
+	printf("\n\t\t\tOR");
+	printf("\nAddress: vijayawada \t AGE : 20 \t gender: male");
+	
+}
