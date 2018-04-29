f8bc697ca1cbb86ab5b2946ebfb9f4410a0aa6a1
potureddigowtham <potureddigowtham@gmail.com>
2018-04-29 19:23:37 +0530

    palindrome.cpp is added

---
+++ palindrome.cpp
@@ -0,0 +1,25 @@
+// palindrome number
+// number should be same even if reversed. ex: 121 , 12321. 
+#include <stdio.h>
+int main()
+{
+	int n,r,s=0;
+	printf("Enter the number :");
+	scanf("%d",&n);
+	int t=n;
+	while(n!=0)
+	{
+		r=n%10;
+		s=s*10+r;
+		n=n/10;
+	}
+	printf("s=%d",s);
+	if(s==t)
+	{
+		printf("\nYes");
+	}
+	else
+	{
+		printf("\nNo");
+	}
+}
