f88a9850a3f5be21a436661df9e17581bdd56262
potureddigowtham <potureddigowtham@gmail.com>
2018-04-29 19:03:28 +0530

    perfect number.cpp is added

---
+++ perfect number.cpp
@@ -0,0 +1,26 @@
+// perfect number 
+// number itself is equal to sum of its proper divisors
+
+#include <stdio.h>
+
+int main()
+{
+	int n,s=0;
+	printf("Enter the number : ");
+	scanf("%d",&n);
+	for(int i=1;i<n;i++)
+	{
+		if(n%i==0)
+		{
+			s=s+i;
+		}
+	}
+	if(s==n)
+	{
+		printf("Yes %d is a Perfect Number.",n);
+	}
+	else
+	{
+		printf("No %d is not a Perfect Number.",n);
+	}
+}
