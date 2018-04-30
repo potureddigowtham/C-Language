7d76b4db827d98c326dc92f709ca1a164341bcfe
potureddigowtham <potureddigowtham@gmail.com>
2018-04-30 22:44:58 +0530

    vowel or not.cpp is added

---
+++ vowel or not.cpp
@@ -0,0 +1,26 @@
+// vowel or not in a sentence
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char sentence[30];
+	int i,flag=0;
+	printf("Enter the sentence : ");
+	scanf("%[^\n]s",sentence);
+//	printf("%c",sentence[4]);
+	for(i=0;i<=30;i++){
+		if(sentence[i]=='a'||sentence[i]=='e'||sentence[i]=='i'||sentence[i]=='o'||sentence[i]=='u')
+		{
+			flag=1;
+		}
+	}
+	if(flag==1)
+	{
+		printf("vowel");
+	}
+	else
+	{
+		printf("nope");	
+	}
+}
