9e89a4907b502856e94d56b2c83bc5a8ed18d72c
potureddigowtham <potureddigowtham@gmail.com>
2018-04-30 23:07:02 +0530

    swapping num.cpp is added

---
+++ swapping num.cpp
@@ -0,0 +1,14 @@
+// swapping of 2 integers without temp varialble
+
+#include <stdio.h>
+
+int main()
+{
+	int a,b;
+	printf("enter values for 2 num :");
+	scanf("%d%d",&a,&b);
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	printf("A = %d B = %d",a,b);
+}
