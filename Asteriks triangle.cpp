3bbc69c6570030753eadd7eed4f514fccbb51b31
potureddigowtham <potureddigowtham@gmail.com>
2018-04-30 23:33:43 +0530

    Asteriks triangle.cpp is added

---
+++ Asteriks triangle.cpp
@@ -0,0 +1,25 @@
+// Asteriks triangle 
+
+#include <stdio.h>
+
+int main()
+{
+	int i,j,n;
+	printf("enter triangle length :");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(i>=j)
+			{
+				printf("*");	
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+}
