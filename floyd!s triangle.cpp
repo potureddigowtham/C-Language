e7ce4f51a527d3b81930fb71b705f2bd0b00a42b
potureddigowtham <potureddigowtham@gmail.com>
2018-04-30 23:28:47 +0530

    floyd!s triangle.cpp is added

---
+++ floyd!s triangle.cpp
@@ -0,0 +1,24 @@
+// floyd!s triangle 
+
+#include <stdio.h>
+
+int main()
+{
+	int i,j;
+	int count=1;
+	for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=4;j++)
+		{
+			if(i>=j)
+			{
+				printf("%d",count++);	
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+}
