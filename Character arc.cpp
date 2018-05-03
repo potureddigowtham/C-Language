9ab20f2bb10163d5059c03d6fa65d699ba0c5ae5
potureddigowtham <potureddigowtham@gmail.com>
2018-05-03 23:04:27 +0530

    Character arc.cpp is added

---
+++ Character arc.cpp
@@ -0,0 +1,18 @@
+// Nested for loop arch with characters
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char input[30];
+	int i;
+	printf("Enter the string :");
+	scanf("%[^\n]s",&input);
+	int n=strlen(input);
+	printf("%d",n);
+	for(i=0;i<=n;i++)
+	{
+		printf("%c",input[i]);	
+	}	
+}
+
