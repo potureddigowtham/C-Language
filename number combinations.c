bfcf3c7a271c165a38b45dfc37da7dd16b1b8d97
potureddigowtham <potureddigowtham@gmail.com>
2018-05-04 07:31:29 +0530

    number combinations.c is added

---
+++ number combinations.c
@@ -0,0 +1,21 @@
+// number combinations
+
+#include <stdio.h>
+
+int main()
+{
+	int n,r,sum=0;
+	int range;
+	printf("Enter numbers for combination :");
+	scanf("%d",&n);
+	printf("Enter the range :");
+	scanf("%d",&range);
+	while(n>0)
+	{
+		r=n%10;
+		printf("\n%d\t%d%d",r,r,r);
+		sum=sum*10+r;
+		n=n/10;
+	}
+//	printf("\n%d",sum);
+}
