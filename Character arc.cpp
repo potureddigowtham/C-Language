916dd050839911e5658955db19cad0a200b6de44
potureddigowtham <potureddigowtham@gmail.com>
2018-05-04 06:40:41 +0530

    Character arc.cpp is updated

--- Character arc.cpp
+++ Character arc.cpp
@@ -5,19 +5,27 @@
 int main()
 {
 	char input[30];
-	int i;
+	int i,j,k;
 	printf("Enter the string :");
 	scanf("%[^\n]s",&input);
 	int n=strlen(input);
-//	printf("%d",n);
-/*	for(i=0;i<=n;i++)
-	{
-		printf("%c",input[i]);	
-	}	
-*/
 	for(i=0;i<=n;i++)
 	{
-			
+			for(j=0;j<n-i;j++)
+			{
+				printf("%c",input[j]);	
+			}
+				for(j=0;j<i*2;j++)
+				{
+					printf(" ");
+				}
+					k=n-i-1;
+					for(j=k;j>=0;j--)
+					{
+						printf("%c",input[j]);		
+					}
+					printf("\n");
+					
 	}	
 }
 
