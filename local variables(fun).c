0c8452fdde68060b904f8decfda602bbb0fb565e
potureddigowtham <potureddigowtham@gmail.com>
2018-05-04 06:49:01 +0530

    local variables(fun).c is added

---
+++ local variables(fun).c
@@ -0,0 +1,28 @@
+//local variables
+
+#include <stdio.h>
+#include <conio.h>
+int fun1();
+int fun2();
+
+int main()
+{
+	int a=1000;
+	fun2();
+	printf("\n%d",a);
+}
+
+int fun1()
+{
+	int a=10;
+	printf("%d",a);
+}
+
+int fun2()
+{
+	fun1();
+	int a=100;
+	printf("\n%d",a);
+}
+
+
